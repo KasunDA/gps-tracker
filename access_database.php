10d6a589d5d5032d44015096932edd9087d0c1b0
Everton Sena <everton.sena@fidelize.com.br>
2018-03-20 20:09:05 -0300

    * Add monolog. * Teste de acesso ao banco

--- access_database.php
+++ access_database.php
@@ -1,7 +1,22 @@
 <?php
+require('../vendor/autoload.php');
 use Silex\Application;
 
 $app = new Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+$app['monolog']->addDebug('logging output.');
+
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
                array(
