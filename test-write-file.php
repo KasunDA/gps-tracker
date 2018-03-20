de0a34045a92c91135d0d4a5e2ce21e249c5527d
Everton Sena <everton.sena@fidelize.com.br>
2018-03-20 19:30:42 -0300

    * Teste

--- test-write-file.php
+++ test-write-file.php
@@ -1,8 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-echo $abc;
-$fp = fopen("bloco1.txt", "a");
+
+$fp = fopen("bloco2.txt", "a");
 
 // Escreve "exemplo de escrita" no bloco1.txt
 $escreve = fwrite($fp, "Teste de escrita em arquivo" . "\n");
